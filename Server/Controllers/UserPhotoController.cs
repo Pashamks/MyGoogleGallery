6acfdda3d56eb96d67c4155e1fb1b0cf53bd01aa
pavlo <pavlo@DESKTOP-SM098C1>
2022-04-13 13:18:29 +0300

    updated the view and added saving pictures to db

---
+++ Server/Controllers/UserPhotoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyGooglegallery.EfCore.Repository;
+using MyGoogleGallery.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGoogleGallery.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPhotoController : ControllerBase
+    {
+        private static EfCoreRepository _efCoreRepository = new EfCoreRepository();
+
+        [HttpPost]
+        [Route("add_photo")]
+        public async Task<UserPhotoModel> AddNewPhoto(UserPhotoModel userPhotoModel)
+        {
+            //_efCoreRepository.AddNewPicture((UserPhoto)userPhotoModel);
+            return userPhotoModel;
+        }
+        [HttpGet]
+        [Route("get_photos")]
+        public async Task<string> GetPhotos()
+        {
+            return "Get";
+            //_efCoreRepository.AddNewPicture((UserPhoto)userPhotoModel);
+        }
+    }
+}
---
+++ Shared/Models/UserPhotoModel.cs
@@ -0,0 +1,21 @@
+﻿using MyGooglegallery.EfCore.Repository.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGoogleGallery.Shared.Models
+{
+    public class UserPhotoModel
+    {
+        public string PhotoUrl { get; set; }
+        public string PhotoType { get; set; }
+        public int SearchingCounter { get; set; } = 0;
+
+        public static  implicit operator UserPhoto(UserPhotoModel userPhotoModel)
+        {
+            return new UserPhoto { PhotoType = userPhotoModel.PhotoType, PhotoUrl = userPhotoModel.PhotoUrl, SearchingCounter = userPhotoModel.SearchingCounter };
+        }
+    }
+}
