ef8e0bd6e2824a16e8886846a989160ba2c736c1
pavlo <pavlo@DESKTOP-SM098C1>
2022-04-15 08:09:50 +0300

    finished add picture page

--- Server/Controllers/UserPhotoController.cs
+++ Server/Controllers/UserPhotoController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyGooglegallery.EfCore.Repository;
+using MyGooglegallery.EfCore.Repository.DbModels;
 using MyGoogleGallery.Shared.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace MyGoogleGallery.Server.Controllers
@@ -17,10 +20,10 @@ namespace MyGoogleGallery.Server.Controllers
 
         [HttpPost]
         [Route("add_photo")]
-        public async Task<UserPhotoModel> AddNewPhoto(UserPhotoModel userPhotoModel)
+        public async Task<UserPhoto> AddNewPhoto(UserPhotoModel userPhotoModel)
         {
-            //_efCoreRepository.AddNewPicture((UserPhoto)userPhotoModel);
-            return userPhotoModel;
+            _efCoreRepository.AddNewPicture((UserPhoto)userPhotoModel);
+            return (UserPhoto)userPhotoModel;
         }
         [HttpGet]
         [Route("get_photos")]
@@ -28,6 +31,28 @@ namespace MyGoogleGallery.Server.Controllers
         {
             return "Get";
             //_efCoreRepository.AddNewPicture((UserPhoto)userPhotoModel);
+        }
+        [HttpGet]
+        [Route("find_photos")]
+        public async Task<string> FindPhotos([FromQuery]string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm))
+                throw new Exception("searchTerm is empty");
+
+            const string subscriptionKey = "26728888-9c5297e15ee95958b38b15abd";
+            const string uriBase = "https://pixabay.com/api/?";
+            var uriQuery = uriBase + "key=" + Uri.EscapeDataString(subscriptionKey) + "&q=" + Uri.EscapeDataString(searchTerm) + "&media_type=photo";
+
+            WebRequest request = WebRequest.Create(uriQuery);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            var jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject<PhotoResponce>(json);
+
+            Random random = new Random();
+            int i = random.Next(0, jsonObject.hits.Count);
+
+            return jsonObject.hits[i].previewURL;
         }
     }
 }
---
+++ Shared/Models/PhotoResponce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGoogleGallery.Shared.Models
+{
+    public class PhotoResponce
+    {
+        public List<PhotoUrlResponce> hits { get; set; }
+    }
+    public class PhotoUrlResponce
+    {
+        public string previewURL { get; set; }
+    }
+}
