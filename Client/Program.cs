154cd445982aea1cd413c87ce64eba5c38005882
pavlo <pavlo@DESKTOP-SM098C1>
2022-04-17 21:11:59 +0300

    added authorization page

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,12 +18,39 @@ namespace MyGoogleGallery.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, TokenAuthenticationStateProvider>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+           
             await builder.Build().RunAsync();
+        }
+    }
+    public class TokenAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        //private string _email { get; set; }
+        //public TokenAuthenticationStateProvider(string email)
+        //{
+        //    _email = email;
+        //}
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //if (string.IsNullOrEmpty(_email))
+            //{
+            //    var anonymousIdentity = new ClaimsIdentity();
+            //    var anonymousPrincipal = new ClaimsPrincipal(anonymousIdentity);
+            //    return new AuthenticationState(anonymousPrincipal);
+            //}
+            var anonymousIdentity = new ClaimsIdentity();
+            var anonymousPrincipal = new ClaimsPrincipal(anonymousIdentity);
+            return new AuthenticationState(anonymousPrincipal);
+            //var claims = new List<Claim> { new Claim(ClaimTypes.Email, "admin@gmail.com") };
+            //var identity = new ClaimsIdentity(claims, "Email");
+            //var principal = new ClaimsPrincipal(identity);
+            //return new AuthenticationState(principal);
+
+
         }
     }
 }
---
+++ Client/Shared/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace MyGoogleGallery.Client.Shared
+{
+    public class LoginModel: ComponentBase
+    {
+        public LoginModel()
+        {
+            LoginData = new UserViewModel();
+        }
+        public UserViewModel LoginData { get; set; }
+        protected Task LoginAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(8,ErrorMessage = "Password is to short!")]
+        public string Password { get; set; }
+    }
+}
